6dfd1d3dc5fcb788650b69fb5d88cfd466c341f2
andreacassini <andrea1316@hotmail.it>
2026-07-15 09:44:54 +0200

    Chatbot structure

---
+++ main.py
@@ -0,0 +1,18 @@
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = openai.OpenAI()
+
+
+while True:
+    text = input("Tu: ")
+    if text == "exit":
+        break
+    response = client.responses.create(
+        model="gpt-5.6",
+        input=text)
+    print(response.output_text)
+    
